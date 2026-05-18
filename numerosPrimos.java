f30fdbce76a63bfe8267f4d066216fdaeed71723
Andrés Felipe Jaimes Martínez <andresfelipeja51@gmail.com>
2026-05-18 09:43:34 +0100

    generando javaDoc

--- numerosPrimos.java
+++ numerosPrimos.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
 
+/**
+ * The type Numeros primos.
+ */
 public class numerosPrimos {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         int ndias = pedirNumeroDias();
         int años = calcularAnios(ndias);
@@ -12,6 +20,11 @@ public class numerosPrimos {
     }
 
 
+    /**
+     * Pedir numero dias int.
+     *
+     * @return the int
+     */
     public static int pedirNumeroDias() {
         Scanner t = new Scanner(System.in);
         int ndias;
@@ -23,18 +36,44 @@ public class numerosPrimos {
         return ndias;
     }
 
+    /**
+     * Calcular anios int.
+     *
+     * @param ndias the ndias
+     * @return the int
+     */
     public static int calcularAnios(int ndias) {
         return ndias / 365;
     }
 
+    /**
+     * Calcular meses int.
+     *
+     * @param ndias the ndias
+     * @return the int
+     */
     public static int calcularMeses(int ndias) {
         return (ndias % 365) / 30;
     }
 
+    /**
+     * Calcular dias int.
+     *
+     * @param ndias the ndias
+     * @return the int
+     */
     public static int calcularDias(int ndias) {
         return (ndias % 365) % 30;
     }
-    
+
+    /**
+     * Mostrar resultado.
+     *
+     * @param ndias the ndias
+     * @param años  the años
+     * @param meses the meses
+     * @param dias  the dias
+     */
     public static void mostrarResultado(int ndias, int años, int meses, int dias) {
         System.out.println("El " + ndias + " corresponde a "
                 + años + " año(s), "
